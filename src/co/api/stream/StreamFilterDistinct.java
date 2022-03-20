1ef2233282b01a44048a7c2481958b2fa2221c82
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 13:35:58 -0500

    Declarative Stream.

--- src/co/api/stream/StreamFilterDistinct.java
+++ src/co/api/stream/StreamFilterDistinct.java
@@ -5,8 +5,10 @@ import java.util.stream.Stream;
 public class StreamFilterDistinct {
 
 	public static void main(String[] args) {
-		Stream.of("User a", "Another User", "Duplicated user!", "w w", "e e", "Duplicated user!", "t t", "y y",
-				"Duplicated user!").distinct().forEach(System.out::println);
+		Stream<String> myStream = Stream.of("User a", "Another User", "Duplicated user!", "w w", "e e",
+				"Duplicated user!", "t t", "y y", "Duplicated user!").distinct();
+
+		myStream.forEach(System.out::println);
 	}
 
 }
