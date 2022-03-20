feb31ad090fe784a3783510ef1f128a0f053056b
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 14:05:33 -0500

    Stream - Reduce Int Sum.

--- src/co/api/stream/StreamFilterReduce.java
+++ src/co/api/stream/StreamFilterReduce.java
@@ -5,11 +5,16 @@ import java.util.stream.Stream;
 public class StreamFilterReduce {
 
 	public static void main(String[] args) {
+
 		Stream<String> myStream = Stream.of("User a", "Another User", "Duplicated user!", "w w", "e e",
 				"Duplicated user!", "t t", "y y", "Duplicated user!").distinct();
 
 		String response = myStream.reduce("Initial value: ", (arg1, arg2) -> arg1 + " | " + arg2);
 		System.out.println(response);
+
+		Stream<Integer> numbersStream = Stream.of(1, 2, 3, 4, 5);
+		int result = numbersStream.reduce(0, (a, b) -> a + b);
+		System.out.println("Result sum numbersStream: " + result);
 	}
 
 }
