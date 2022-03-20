f987d73ec7ec21f738586d4dd5ce68f937265b62
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 12:27:42 -0500

    Stream - filter.

--- src/co/api/stream/StreamFilter.java
+++ src/co/api/stream/StreamFilter.java
@@ -10,18 +10,21 @@ public class StreamFilter {
 	public static void main(String[] args) {
 
 		System.out.println("------------------");
-		Stream<User> usersStream = Stream
-				.of("User1 Lastname1", "Customer Lastname2", "User3 Lastname3", "User4 Lastname4", "Customer Lastname5")
-				.map(fullName -> new User(fullName.split(" ")[0], fullName.split(" ")[1]))
-				.filter(e -> e.getName().toLowerCase().equals("customer")).peek(System.out::println);
+		Stream<User> usersStream = Stream.of("1 1", "2 2", "3 3").map(u -> new User(u.split(" ")[0], u.split(" ")[1]))
+				.filter(e -> e.getName().toLowerCase().equals("2")).peek(System.out::println);
 		List<User> listFromUsersStream2 = usersStream.collect(Collectors.toList());
 		listFromUsersStream2.forEach(System.out::println);
 
 		System.out.println("------------------");
-		User myUserMapFilterFindFirst = Stream
-				.of("User1 Lastname1", "Customer Lastname2", "User3 Lastname3", "User4 Lastname4", "Customer Lastname5")
-				.map(u -> new User(u.split(" ")[0], u.split(" ")[1]))
-				.filter(u -> u.getName().toLowerCase().equals("user3")).findFirst().get();
+		User myUserMapFilterFindFirst = Stream.of("4 4", "5 5", "6 6")
+				.map(u -> new User(u.split(" ")[0], u.split(" ")[1])).filter(u -> u.getName().toLowerCase().equals("5"))
+				.findFirst().get();
 		System.out.println(myUserMapFilterFindFirst);
+
+		System.out.println("------------------");
+		User userFilterById = Stream.of("7 7", "8 8").peek(System.out::println)
+				.map(u -> new User(u.split(" ")[0], u.split(" ")[1])).filter(u -> u.getId().equals(6)).findFirst()
+				.get();
+		System.out.println(userFilterById);
 	}
 }
--- src/co/models/User.java
+++ src/co/models/User.java
@@ -4,10 +4,13 @@ public class User {
 
 	private String name;
 	private String lastName;
+	private Integer id;
+	private static int countId;
 
 	public User(String name, String lastName) {
 		this.name = name;
 		this.lastName = lastName;
+		this.id = ++countId;
 	}
 
 	public String getName() {
@@ -26,9 +29,17 @@ public class User {
 		this.lastName = lastName;
 	}
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
 	@Override
 	public String toString() {
-		return name + " " + lastName;
+		return name + " " + lastName + " - id:" + id;
 	}
 
 }
