fff32c5ef8ebf2e5164d9da3b12da98a4045c6aa
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 13:22:25 -0500

    Optional - isPreset()

--- src/co/api/stream/OptionalEx.java
+++ src/co/api/stream/OptionalEx.java
@@ -8,9 +8,9 @@ import co.models.User;
 public class OptionalEx {
 
 	public static void main(String[] args) {
-		Stream<User> users = Stream.of("q q", "w w", "e e", "r r", "t t", "y y")
-				.map(data -> new User(data.split("")[0], data.split("")[1]))
-				.filter(element -> element.getName().equals("User Not found!")).peek(System.out::println);
+		Stream<User> users = Stream.of("q q", "w w", "e e", "r r", "t t", "y y", "oK Ok")
+				.map(data -> new User(data.split(" ")[0], data.split(" ")[1]))
+				.filter(element -> element.getName().toLowerCase().equals("ok")).peek(System.out::println);
 
 		Optional<User> myOptionalUser = users.findFirst();
 
@@ -19,7 +19,12 @@ public class OptionalEx {
 		// "User")));
 		// System.out.println("OUT: " + myOptionalUser.orElseGet(() -> new
 		// User("Default", "User")));
-		System.out.println("OUT: " + myOptionalUser.orElseThrow(null));
+		// System.out.println("OUT: " + myOptionalUser.orElseThrow(null));
+		if (myOptionalUser.isPresent()) {
+			System.out.println(myOptionalUser.get().getName().toUpperCase());
+		} else {
+			System.out.println("User Not Found!");
+		}
 	}
 
 }
