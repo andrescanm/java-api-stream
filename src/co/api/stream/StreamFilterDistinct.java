6d9f6e72e553e6af6206f0ef9ad63b2eba960fce
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 13:42:32 -0500

    Stream to List. -- Output: Iteration listFromStream: User a Iteration listFromStream: Another User Iteration listFromStream: Duplicated user! Iteration listFromStream: w w Iteration listFromStream: e e Iteration listFromStream: t t Iteration listFromStream: y y

--- src/co/api/stream/StreamFilterDistinct.java
+++ src/co/api/stream/StreamFilterDistinct.java
@@ -1,5 +1,7 @@
 package co.api.stream;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamFilterDistinct {
@@ -8,7 +10,12 @@ public class StreamFilterDistinct {
 		Stream<String> myStream = Stream.of("User a", "Another User", "Duplicated user!", "w w", "e e",
 				"Duplicated user!", "t t", "y y", "Duplicated user!").distinct();
 
-		myStream.forEach(System.out::println);
+		// myStream.forEach(System.out::println);
+
+		List<String> listFromStream = myStream.collect(Collectors.toList());
+		for (String element : listFromStream) {
+			System.out.println("Iteration listFromStream: " + element);
+		}
 	}
 
 }
