934c0d6a0357d6497ad4d62122f9b0eb18966cd4
Andrés Cañón <mario.canon@datatools.com.co>
2022-03-20 13:12:23 -0500

    Optional - orElseGet()

--- src/co/api/stream/OptionalEx.java
+++ src/co/api/stream/OptionalEx.java
@@ -10,12 +10,14 @@ public class OptionalEx {
 	public static void main(String[] args) {
 		Stream<User> users = Stream.of("q q", "w w", "e e", "r r", "t t", "y y")
 				.map(data -> new User(data.split("")[0], data.split("")[1]))
-				.filter(element -> element.getName().equals("ZZZZZ")).peek(System.out::println);
+				.filter(element -> element.getName().equals("User Not found!")).peek(System.out::println);
 
 		Optional<User> myOptionalUser = users.findFirst();
 
 		// System.out.println("OUT: " + myOptionalUser.get());
-		System.out.println("OUT: " + myOptionalUser.orElse(new User("Default", "User")));
+		// System.out.println("OUT: " + myOptionalUser.orElse(new User("Default",
+		// "User")));
+		System.out.println("OUT: " + myOptionalUser.orElseGet(() -> new User("Default", "User")));
 	}
 
 }
